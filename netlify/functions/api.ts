f920ba521366fbe0fc1cb1bc81c60f3bc066585c
jrcumminsent <jrcumminsent@gmail.com>
2026-08-28 22:15:51 -0500

    Install A2A402 Network routes in Netlify runtime

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,6 +4,7 @@ import {
 } from "@netlify/database";
 
 import { buildApp } from "../../apps/api/src/app.js";
+import { installNetworkRoutes } from "../../apps/api/src/network.js";
 import { createNetlifyArtifactStorage } from "./blob-storage.js";
 
 let application: Promise<Awaited<ReturnType<typeof buildApp>>> | undefined;
@@ -20,6 +21,9 @@ function app(): Promise<Awaited<ReturnType<typeof buildApp>>> {
   }
   application ??= buildApp({
     artifactStorage: createNetlifyArtifactStorage(10_000_000),
+  }).then((context) => {
+    installNetworkRoutes(context.server, context.engine);
+    return context;
   });
   return application;
 }
