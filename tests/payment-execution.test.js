008f7abe192db87e85ec5c35fd45c1eea693d4f1
jrcumminsent <jrcumminsent@gmail.com>
2026-09-18 21:19:00 -0500

    Update payment intent fixture for multichain config

--- tests/payment-execution.test.js
+++ tests/payment-execution.test.js
@@ -6,6 +6,7 @@ import { paymentIntentForJob, pendingPaymentIntents, transactionHashAlreadyUsed 
 const config = {
   baseUrl: 'https://a2a402.market',
   tokenAddress: '0xF2bb6DC14E9097EC08F9Eaa9C6B7d39662195F01',
+  usdcAddresses: { base:'0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913' },
   treasuryAddress: '0x3333333333333333333333333333333333333333'
 };
 
